e8eb31bd5ea670d10969f2aa2327c68556cd4c45
Sragvi vadali <sragvivadali@Sragvis-MacBook-Pro.local>
2023-04-22 11:53:30 -0700

    api call

---
+++ user.py
@@ -0,0 +1,29 @@
+class User:
+    personality = {
+        "Thrillers": False,
+        "InvisibleHopper" : False,
+        "CityHustler" : False,
+        "LoneWolf" : False,
+        "TypicalTraveller" : False,
+        "SocialMediaCrazy" : False,
+        "LoveBirds" : False,
+        "Hippie" : False,
+        "Foodie" : False,
+        "NightOwl" : False
+    }
+
+    description = {
+        "Thrillers": "adrenaline activities",
+        "InvisibleHopper" : "relaxing",
+        "CityHustler" : "city activities",
+        "LoneWolf" : "secluded areas",
+        "TypicalTraveller" : "tourist spot",
+        "SocialMediaCrazy" : "picture places",
+        "LoveBirds" : "romantic activities",
+        "NatureLover" : "natural places",
+        "Foodie" : "good local food",
+        "NightOwl" : "night life"
+    }
+
+    def toggleType(self, typeName) -> bool:
+        self.personality[typeName] = not self.personality[typeName]
